5058f27a6b02569b2a071e2b65e3845d540cf01d
gengyuanmax <33713574+gengyuanmax@users.noreply.github.com>
2023-03-25 05:37:38 +0100

    set static root

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'littlelemon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'restaurant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = [
     'restaurant/static/'
